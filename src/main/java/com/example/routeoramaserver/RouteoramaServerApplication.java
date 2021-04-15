a3f10e4ba0dea98740d5dd64cb41d540fa32d2a4
weilderbeast <nu_imi_pasa_01>
2021-04-15 11:37:44 +0300

    Initial Commit

---
+++ src/main/java/com/example/routeoramaserver/RouteoramaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.routeoramaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteoramaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RouteoramaServerApplication.class, args);
+    }
+
+}
