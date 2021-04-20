2d81c7aab47d25f9aa83e5d4d13f6668ace0c969
Malgorzata Drygala <49656312+gosiadrygala@users.noreply.github.com>
2021-04-20 22:47:52 +0200

    Quick fix

--- src/main/java/com/example/routeoramaserver/controllers/login/LoginController.java
+++ src/main/java/com/example/routeoramaserver/controllers/login/LoginController.java
@@ -3,10 +3,8 @@ package com.example.routeoramaserver.controllers.login;
 import com.example.routeoramaserver.controllers.login.rmi.ILoginClient;
 import com.example.routeoramaserver.controllers.login.rmi.LoginClient;
 import com.example.routeoramaserver.models.User;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -20,7 +18,8 @@ public class LoginController {
 
     @PostMapping(value = "/login", consumes = "application/json", produces = "application/json")
     public User Login(@RequestBody User user){
-        return loginClient.Login(user.getUsername(), user.getPassword());
+        User returnedUser = loginClient.Login(user.getUsername(), user.getPassword());
+        return returnedUser;
     }
 
     // ask about the use of a session ID
